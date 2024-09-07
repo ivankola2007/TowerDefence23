f03d7c330d7cacbd6c6add36350a5275697c2c2d
Ivankola <ivankovriha@gmail.com>
2024-09-07 12:43:19 +0300

    Lesson1

---
+++ TowerDefence/Assets/New Folder/GameController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+
+{
+    public const int NODE_GRID_SIZE = 4;
+    private GameObject nodePrefab;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void CreateNodes()
+    {
+       for (int z= 0; z <NODE_GRID_ROW_COUNT; z++)
+    }
+        
+    for (int i = 0; i < 16; i++)
+
+    {
+        
+    }
+
+}
