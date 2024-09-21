a2900c6ae7d1e1420e60d81f1a1b51415761a380
Ivankola <ivankovriha@gmail.com>
2024-09-21 12:40:52 +0300

    Lesson (виправити помилку не забуду)

--- TowerDefence/Assets/New Folder/LevelCreater.cs
+++ TowerDefence/Assets/New Folder/LevelCreater.cs
@@ -2,14 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameController : MonoBehaviour
+public class LevelCreator : MonoBehaviour
 {
     public  int NODE_GRID_ROW_COUNT = 4;
     public  int NODE_GRID_COLUMN_COUNT = 4;
     [SerializeField]
+    [Space(15)]
+    [SerielizeField]
     private GameObject nodePrefab;
+    [SerializeField]
+
+     private GameObject planePrefab;
+     [Space(15)]
+     [SerializeField]
+
     // Start is called before the first frame update
-    private void Start()
+   
     {
         CreateNodes();
     }
@@ -22,8 +30,13 @@ public class GameController : MonoBehaviour
          for (int z= 0; z < NODE_GRID_COLUMN_COUNT; z++)
         
         Instantiate(nodePrefab, new Vector3(x, 0, z), Quaternion.identity);
-
-    
+        GameObject plane = Instantiate(plane Prefab, new Vector3 ((NODE GRID ROW COUNT = offset)/2-1, 0,(NODE GRID_COLUMN COUNT * offset)/ 2-1), Quaternion. identity, nodeParent); 
+        plane.transform.localScale = new Vector3(0.2f * NODE GRID ROW COUNT, plane.transform.localScale.y,0.2f * NODE GRID COLUMN_COUNT);
+        for (int x; NODE GRIO_ROM COUNT; x++)
+        for (int zB z NODE GRID COLUMN COUNT; z++)
+        GameObject obj Instantiate(nodePrefab, new Vector3(x * offset, 8, z offset),Quaternion.identity, nodeParent);
+        obj.name"Node: = + x + "  +z);
+         
     }
     
 }
